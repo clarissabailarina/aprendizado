733f302970807aaccf8998fb0b52ea66a5184eab
Clarissa Ferreira <Clarissa Ferreira>
2017-02-21 16:37:58 -0300

    meus primeiros progamas

---
+++ calculadora.py
@@ -0,0 +1,6 @@
+"""Este progama soma dois numeros o primeiro e o segundo"""
+
+print('Ola! Bem vindo a calculadora da Clarissa')
+primeiro = input('Digite o primeiro numero')
+segundo = input('Digite o segundo numero')
+print(int(primeiro) + int(segundo))
